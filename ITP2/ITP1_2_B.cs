f995e9774b62630fbfe49619e60ffa107b3da328
Hikaru-28 <hikaru.taknouch@gmail.com>
2018-12-15 10:37:47 +0900

    Create ITP1_2_B.cs

---
+++ ITP2/ITP1_2_B.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace ITP1_2_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x = Console.ReadLine().Split();
+            int a = int.Parse(x[0]);
+            int b = int.Parse(x[1]);
+            int c = int.Parse(x[2]);
+
+            if (a < b && b< c)
+            {
+                Console.WriteLine("Yes");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
