27bbd2bc7f4b291c686a1e333ee161072f1aca7e
Hikaru-28 <hikaru.taknouch@gmail.com>
2018-12-18 00:11:38 +0900

    Create ITP1_3_A.cs

---
+++ ITP3/ITP1_3_A.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ITP1_3_A
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 1000; i ++)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
