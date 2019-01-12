c8a9168c65e4d36b83dc1fd42fa618ae9e64d023
Hikaru-28 <hikaru.taknouch@gmail.com>
2019-01-12 16:44:34 +0900

    ITP1_6_C解けまぴた！！

--- ITP1_6/ITP1_6_C.cs
+++ ITP1_6/ITP1_6_C.cs
@@ -7,35 +7,30 @@ namespace ITP1_6_C
     static void Main()
     {
       int n = int.Parse(Console.ReadLine());
-
-      for (int i = 0; i < 3; i ++)
+      int[, ,] A = new int[4, 3, 10];
+      for (int i = 0; i < n; i ++)
       {
-        string[] x = Console.ReadLine().Split(' ');
-        int b = int.Parse(x[0]); //0<b<=4
-        int f = int.Parse(x[1]); //0<f<=3
-        int r = int.Parse(x[2]); //0<r<=10
-        int v = int.Parse(x[3]);
+        string[] inputs = Console.ReadLine().Split(' ');
+        int b = int.Parse(inputs[0]); //0<b<=4
+        int f = int.Parse(inputs[1]); //0<f<=3
+        int r = int.Parse(inputs[2]); //0<r<=10
+        int v = int.Parse(inputs[3]);
         //b棟f階のr番目の部屋にv人が追加で入居したことを示す。
-        for (int j = 0; j < 2; j ++)
+        A[b - 1, f - 1, r - 1] = v;
+      }
+      for (int i = 0; i < 4; i ++)
+      {
+        for (int j = 0; j < 3; j ++)
         {
           for (int k = 0; k < 10; k ++)
           {
-            if (if i = f && j = r)
-            {
-              Console.Write(" " + v.ToString);
-            }
-            else
-            {
-              Console.Write(" 0");
-            }
-          }
-          Console.WriteLine();
-          for (int l = 0; l < 20; l ++)
-          {
-            Console.Write("#");
+            if (A[i, j, k] == null) A[i, j, k] = 0;
+            Console.Write(" {0}", A[i, j, k]);
           }
           Console.WriteLine();
         }
+        string text = new String('#', 20);
+        if (i != 3) Console.WriteLine(text);
       }
     }
   }
