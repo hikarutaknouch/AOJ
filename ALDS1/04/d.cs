76039f18610f063c6bef16c96ba6ab8dcf8adc31
hikarutaknouch <hikaru.taknouch@gmail.com>
2019-03-28 22:54:30 +0900

    alds04

---
+++ ALDS1/04/d.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args) {
+        int[] s = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = s[0];
+        int k = s[1];
+    }
+}
